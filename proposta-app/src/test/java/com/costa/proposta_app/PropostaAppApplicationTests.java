d020e2c24d5682b0913ec8ff1d42d0a9317e5140
Thiago Ribeiro <trcosta97@hotmail.com>
2024-12-11 08:16:07 -0300

    base do projeto

---
+++ proposta-app/src/test/java/com/costa/proposta_app/PropostaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.costa.proposta_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
